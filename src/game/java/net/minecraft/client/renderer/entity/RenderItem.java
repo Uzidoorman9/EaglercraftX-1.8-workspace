010858ea11aec80780a3a5e1205c74daf999609f
Uzi <timothymay2011@gmail.com>
2026-04-23 22:56:32 -0400

    Refresh! + new stuff: modified src/game/java/net/minecraft/client/renderer/entity/RenderItem.java

--- src/game/java/net/minecraft/client/renderer/entity/RenderItem.java
+++ src/game/java/net/minecraft/client/renderer/entity/RenderItem.java
@@ -1105,6 +1105,8 @@ public class RenderItem implements IResourceManagerReloadListener {
 		this.registerBlock(Blocks.red_mushroom_block, BlockHugeMushroom.EnumType.ALL_INSIDE.getMetadata(),
 				"red_mushroom_block");
 		this.registerBlock(Blocks.dragon_egg, "dragon_egg");
+    //JJK MOD BELOW HERE
+    this.registerItem(Items.item_sukuna_finger, "item_sukuna_finger"))
 	}
 
 	public void onResourceManagerReload(IResourceManager var1) {
